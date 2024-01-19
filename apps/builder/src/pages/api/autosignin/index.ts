5effb262a6c628a4091353c70b2bb512a46b7f2c
root <root@dev13.instantsandbox.net>
2024-01-19 16:21:53 -0300

    removing log

--- apps/builder/src/pages/api/autosignin/index.ts
+++ apps/builder/src/pages/api/autosignin/index.ts
@@ -3,7 +3,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { getAuthOptions } from '../auth/[...nextauth]'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('inicio')
   let restricted: 'rate-limited' | undefined
   console.log('chave: ', req.headers.customkey)
   const ret = await NextAuthApiAutomaticHandler(
