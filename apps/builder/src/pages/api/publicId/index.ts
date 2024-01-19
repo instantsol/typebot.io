9caab6adb23be339f85c4d18a050a32299789605
root <root@dev13.instantsandbox.net>
2024-01-19 16:04:22 -0300

    adding more functionalities to endpoint publicId

--- apps/builder/src/pages/api/publicId/index.ts
+++ apps/builder/src/pages/api/publicId/index.ts
@@ -3,6 +3,16 @@ import { checkPermission } from '../autosignin/subauth'
 import prisma from '@typebot.io/lib/prisma'
 
 
+async function GetUser(workspaceId: string){
+  const workspace = await prisma.memberInWorkspace.findFirst({
+    where: {
+      workspaceId: workspaceId
+    }
+  })
+  const user = await prisma.user.findFirst({where: {id:  workspace?.userId}})
+  return user?.email
+}
+
 async function TypeBot(key: string) {
   const Bots = await prisma.typebot.findFirst({
     where: {
@@ -12,9 +22,19 @@ async function TypeBot(key: string) {
   return Bots
 }
 
+async function GetById(key: string) {
+  const Bots = await prisma.typebot.findFirst({
+    where: {
+      id: key,
+    },
+  })
+  return Bots
+}
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { ...query } = req.query
   const publicId: string = query?.publicId ? query.publicId.toString() : ''
+  const Id: string = query?.Id ? query.Id.toString() : ''
   try {
     const customkey :string = req.headers.customkey ? req.headers.customkey.toString() : ''
 
@@ -26,10 +46,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(407).json({ botid: null })
   }
   
-  const bots = await TypeBot(publicId)
-  if (bots) {
-    res.status(200).json({ bots: bots.id })  
+  if (publicId){
+    const bots = await TypeBot(publicId)
+    
+    if (bots) {
+      const user = await GetUser(bots?.workspaceId)
+      res.status(200).json({ bots: bots.id, user: user })  
+    }
   }
+  else if (Id){
+    const bots = await GetById(Id)
+
+    if (bots) {
+      const user = await GetUser(bots?.workspaceId)
+      res.status(200).json({ bots: bots.id, user: user })  
+    }
+  }
+
 }
 
 export default handler
