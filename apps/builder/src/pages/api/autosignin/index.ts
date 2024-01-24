51201ea2f156ddcaba9b526b0ad887c374d92ef7
Lucas Instant <lucas.batista@instant.com.br>
2024-01-24 14:20:41 -0300

    adding target_blank to google spreadsheets

--- apps/builder/src/pages/api/autosignin/index.ts
+++ apps/builder/src/pages/api/autosignin/index.ts
@@ -4,7 +4,6 @@ import { getAuthOptions } from '../auth/[...nextauth]'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   let restricted: 'rate-limited' | undefined
-  console.log('chave: ', req.headers.customkey)
   const ret = await NextAuthApiAutomaticHandler(
     req,
     res,
