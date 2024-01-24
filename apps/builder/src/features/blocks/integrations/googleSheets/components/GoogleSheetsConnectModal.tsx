9727c4120b492bbadd45157aac1985b0d532139e
Lucas Instant <lucas.batista@instant.com.br>
2024-01-24 14:22:23 -0300

    linter changes

--- apps/builder/src/features/blocks/integrations/googleSheets/components/GoogleSheetsConnectModal.tsx
+++ apps/builder/src/features/blocks/integrations/googleSheets/components/GoogleSheetsConnectModal.tsx
@@ -69,7 +69,7 @@ export const GoogleSheetConnectModal = ({
                 typebotId
               )}
               mx="auto"
-              target='_blank'
+              target="_blank"
             >
               Continue with Google
             </Button>
