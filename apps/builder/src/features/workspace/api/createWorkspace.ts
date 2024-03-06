57eab9827a00291241f68bb8623e88f2f72daefc
Dilceu Pazinatto <dilceu.pazinatto@instant.com.br>
2024-03-06 16:55:50 -0300

    create default Instant AIO provider on created workspace

--- apps/builder/src/features/workspace/api/createWorkspace.ts
+++ apps/builder/src/features/workspace/api/createWorkspace.ts
@@ -5,6 +5,8 @@ import { Workspace, workspaceSchema } from '@typebot.io/schemas'
 import { z } from 'zod'
 import { parseWorkspaceDefaultPlan } from '../helpers/parseWorkspaceDefaultPlan'
 import { trackEvents } from '@typebot.io/lib/telemetry/trackEvents'
+import { listCredentials } from '@/features/forge/api/credentials/listCredentials'
+import { createCredentials } from '@/features/forge/api/credentials/createCredentials'
 
 export const createWorkspace = authenticatedProcedure
   .meta({
@@ -61,6 +63,30 @@ export const createWorkspace = authenticatedProcedure
         plan,
       },
     })) as Workspace
+
+    /*
+    create default Instant provider
+     */
+    const exist = await listCredentials({
+      workspaceId: newWorkspace.id,
+      userId: user.id,
+      type: 'instantchat',
+    })
+
+    if (!exist.credentials || exist.credentials.length === 0) {
+      const host = user.email.split('@')[0]
+      await createCredentials({
+        credentials: {
+          data: {
+            baseUrl: 'https://' + host,
+            name: 'Instant AIO',
+            type: 'instantchat',
+            workspaceId: newWorkspace.id,
+          },
+        },
+        user,
+      })
+    }
 
     await trackEvents([
       {
