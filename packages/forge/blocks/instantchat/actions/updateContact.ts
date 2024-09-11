b09d9ecac2ae663498bae7faf90fa25125d04793
Héron Silva <heronsilva@instant.com.br>
2024-09-11 15:04:31 -0300

    Add new command to update contact data

---
+++ packages/forge/blocks/instantchat/actions/updateContact.ts
@@ -0,0 +1,55 @@
+import { createAction, option } from '@typebot.io/forge'
+import { auth } from '../auth'
+
+export const updateContact = createAction({
+  auth,
+  name: 'Atualizar Contato',
+  options: option.object({
+    internal_code: option.string.layout({
+      label: 'Código Interno (KID)'
+    }),
+    name: option.string.layout({
+      label: 'Nome'
+    }),
+    telephone: option.string.layout({
+      label: 'Telefone'
+    }),
+    email: option.string.layout({
+      label: 'E-mail'
+    }),
+    enterprise_name: option.string.layout({
+      label: 'Empresa'
+    }),
+    enterprise_cnpj: option.string.layout({
+      label: 'CPF/CNPJ'
+    }),
+    customer_code: option.string.layout({
+      label: 'Código do cliente'
+    })
+  }),
+  run: {
+    server: async ({
+      options: { internal_code, name, telephone, email, enterprise_name, enterprise_cnpj, customer_code },
+      variables,
+      credentials,
+    }) => {
+      const { baseUrl } = credentials
+      const response = await fetch(`${baseUrl}/ivci/webhook/update_contact`, { 
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          platform        : variables.list().find((v) => v.name === 'is_platform')?.value,
+          client_id       : variables.list().find((v) => v.name === 'is_clientid')?.value,
+          accountcode     : variables.list().find((v) => v.name === 'is_accountcode')?.value,
+          internal_code   : internal_code,
+          name            : name,
+          telephone       : telephone,
+          email           : email,
+          enterprise_name : enterprise_name,
+          enterprise_cnpj : enterprise_cnpj,
+          customer_code   : customer_code,
+        })
+      })
+    },
+  }
+})
